db62fa0a6ce32810c02d51b53fb25006eddf310b
Felipe Lara <68015575+larafe1@users.noreply.github.com>
2022-02-13 19:26:44 -0300

    WIP: Workspace cfg

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Text, View } from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar style="dark" />
+      <Text>Hello world</Text>
+    </View>
+  );
+}
